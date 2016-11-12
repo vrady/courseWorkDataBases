d50b75885e4e961686b6b87ce713c888e00dc102
Dima Vrady <wiggledima@gmail.com>
2016-11-12 16:23:48 +0200

    add Materialize

--- src/courseWorkDataBases/Gruntfile.js
+++ src/courseWorkDataBases/Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function (grunt) {
         uglify: {
             my_target: {
                 files: {
-                    'wwwroot/app.js': ['scripts/app.js', 'scripts/**/*.js']
+                    'wwwroot/js/app.js': ['scripts/app.js', 'scripts/**/*.js']
                 }
             }
         },
