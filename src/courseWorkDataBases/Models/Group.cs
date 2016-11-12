c869b1cccbf347de7d759de0b2ccc09981321248
mrgl1st <kovanov1997@gmail.com>
2016-11-12 21:05:23 +0200

    added speciality

--- src/courseWorkDataBases/Models/Group.cs
+++ src/courseWorkDataBases/Models/Group.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace courseWorkDataBases.Models
 {
@@ -9,5 +10,8 @@ namespace courseWorkDataBases.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int Course { get; set; }
+        public DbSet<Speciality> Speciality { get; set; }
+        public int Quantity { get; set; }
     }
 }
