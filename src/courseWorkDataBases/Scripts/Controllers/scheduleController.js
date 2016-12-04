bec61d4d3d8f684c824f81eb3b937871a3a812c4
Dima Vrady <wiggledima@gmail.com>
2016-12-04 20:44:16 +0200

    almost done

--- src/courseWorkDataBases/Scripts/Controllers/scheduleController.js
+++ src/courseWorkDataBases/Scripts/Controllers/scheduleController.js
@@ -5,38 +5,39 @@
         .module('scheduleKpi')
         .controller('scheduleController', scheduleController);
 
-    scheduleController.$inject = ['$scope', 'Schedule', 'Group', 'Teacher', 'Subject', 'Audience', 'orderByFilter', '$route', '$routeParams'];
-    function scheduleController($scope, Schedule, Group,Teacher,Subject, Audience, orderByFilter, $route, $routeParams) {
-        $scope.group = Group.get({ id: $routeParams.id });
+    scheduleController.$inject = ['$scope', 'Schedule', 'Group', 'Teacher','Plan', 'Subject', 'Audience', 'orderByFilter', '$route', '$routeParams'];
+    function scheduleController($scope, Schedule, Group, Teacher, Plan, Subject, Audience, orderByFilter, $route, $routeParams) {
+        $scope.group = Group.get({ id: $routeParams.id }, function (res) {
+            Plan.query({ id: res.specialityId }, function (res) {
+                $scope.plans = res.filter(function (el) {
+                    return Math.floor(el.semester + 1) / 2 == $scope.group.course;
+                })
+            });
+        });
         $scope.weeks = Schedule.query({ id: $routeParams.id });
         $scope.groups = Group.query();
         $scope.subjects = Subject.query();
         $scope.teachers = Teacher.query();
         $scope.audiences = Audience.query();
 
-        console.log($scope.weeks);
-
         $scope.newLesson = new Schedule();
-        $scope.showEditLesson = function (number, day, id) {
-            console.log(number, day);
+        $scope.showEditLesson = function (week, number, day, id) {
             $scope.newLesson.lessonNumber = number;
             $scope.newLesson.day = day;
             $scope.newLesson.groupId = $routeParams.id;
             $scope.newLesson.id = id;
+            $scope.newLesson.week = week;
             $scope.editLesson = function () {
-                console.log($scope.newLesson);
-                
                 $scope.newLesson.$save(function () {
                     $route.reload();
                 });
             }
         }
         
-        $scope.deleteLesson = function (deletedLesson, row) {
+        $scope.deleteLesson = function (deletedLesson, days) {
             $scope.deletedLesson = deletedLesson;
-            $scope.delRow = row;
-            console.log($scope.delRow);
-            $scope.delRow.$remove({ id: $scope.deletedLesson.scheduleId }, function () {
+            $scope.delDays = days;
+            $scope.delDays.$remove({ id: $scope.deletedLesson.scheduleId }, function () {
                 $route.reload();
             })
         }
