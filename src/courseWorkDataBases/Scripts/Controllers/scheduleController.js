a095385965c3264aae748b62e14bc1ff5fd920e3
Dima Vrady <wiggledima@gmail.com>
2016-12-04 17:21:40 +0200

    fdf

--- src/courseWorkDataBases/Scripts/Controllers/scheduleController.js
+++ src/courseWorkDataBases/Scripts/Controllers/scheduleController.js
@@ -25,17 +25,23 @@
             $scope.newLesson.id = id;
             $scope.editLesson = function () {
                 console.log($scope.newLesson);
+                
                 $scope.newLesson.$save(function () {
                     $route.reload();
                 });
             }
         }
         
-        $scope.deleteLesson = function (deletedLesson) {
+        $scope.deleteLesson = function (deletedLesson, row) {
             $scope.deletedLesson = deletedLesson;
-            $scope.deletedLesson.$remove({id: $scope.deletedLesson.id},function () {
+            $scope.delRow = row;
+            console.log($scope.delRow);
+            $scope.delRow.$remove({ id: $scope.deletedLesson.scheduleId }, function () {
                 $route.reload();
             })
+            //$scope.days.$remove({id: $scope.deletedLesson.scheduleId}, function () {
+            //    $route.reload();
+            //})
         }
         
     }
